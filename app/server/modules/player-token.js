48569102a4b0887862aeb98956327b2c6a9f96ee
Kuu Miyazaki <miyazaqui@gmail.com>
2017-06-16 02:16:18 +0900

    Update URL

--- app/server/modules/player-token.js
+++ app/server/modules/player-token.js
@@ -1,7 +1,7 @@
 var config = require('config');
 var OoyalaApi = require('ooyala-api');
 
-var api = new OoyalaApi(config.api.key, config.api.secret);
+var api = new OoyalaApi(config.api.key, config.api.secret, {expirationTime: 180});
 
 exports.get = function(embedCode, accountId) {
   return api.getTokenRequest(embedCode, accountId);
